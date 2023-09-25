c49046ee8614a5589a0e44ddd663ad4a48462159
Bernardo Stein <stein.bernardo@proton.me>
2023-09-25 15:01:24 -0300

    feat: created totalValues section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { AppFinanceForm } from './components/Form';
 import { AppHeader } from './components/Header';
+import { TotalValues } from './components/Total';
 
 const App = () => {
   return (
     <>
       <AppHeader />
       <AppFinanceForm />
+      <TotalValues/> 
     </>
   );
 };
---
+++ src/components/Total/index.jsx
@@ -0,0 +1,12 @@
+export const TotalValues = () => {
+  return (
+    <>
+      <section>
+        <div>
+          <h3>Valor total:</h3><span>R$ 8184,00</span>
+        </div>
+        <small>O valor se refere ao saldo</small>
+      </section>
+    </>
+  )
+}
