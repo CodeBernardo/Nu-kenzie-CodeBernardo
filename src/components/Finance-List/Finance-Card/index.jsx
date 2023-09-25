6024d2170836a69737a4c852ca4d0c57d7785c2a
Bernardo Stein <stein.bernardo@proton.me>
2023-09-25 17:33:16 -0300

    style: created basic balance section styles

--- src/components/Finance-List/Finance-Card/index.jsx
+++ src/components/Finance-List/Finance-Card/index.jsx
@@ -1,11 +1,13 @@
+import styles from "./index.module.scss"
+
 export const FinanceCard = () => {
   return (
     <>
-      <li>
-        <h3>Salário - Mês Dezembro</h3>
-        <small>Entrada</small>
-        <p>R$ 6.660,00</p>
-        <button>Excluir</button>
+      <li className={styles.card__container} >
+        <h3 className="title-3">Salário - Mês Dezembro</h3>
+        <small className="text-r">Entrada</small>
+        <p className="head-line">R$ 6.660,00</p>
+        <button className="text-r">Excluir</button>
       </li>
     </>
   )
--- src/components/Finance-List/index.jsx
+++ src/components/Finance-List/index.jsx
@@ -1,12 +1,13 @@
-import { FinanceCard } from "./Finance-Card"
+import { FinanceCard } from './Finance-Card';
+import styles from './index.module.scss';
 
 export const FinanceList = () => {
   return (
-    <section>
-      <h2>Resumo financeiro</h2>
+    <section className={styles.balance__section}>
+      <h2 className="title-3">Resumo financeiro</h2>
       <ul>
-        <FinanceCard /> 
+        <FinanceCard />
       </ul>
     </section>
-  )
-}
+  );
+};
