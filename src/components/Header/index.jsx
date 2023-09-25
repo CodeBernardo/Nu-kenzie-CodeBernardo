41996629137daaec4e23988d2672f71ede551e73
Bernardo Stein <stein.bernardo@proton.me>
2023-09-25 16:24:07 -0300

    style: created basic header styles

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,10 @@
+import styles from "./index.module.scss"
+
 export const AppHeader = () => {
   return (
-    <h1><span>Nu</span> Kenzie</h1>
+    <header className={styles.header}>
+      <h1 className={`title-2 ${styles.nu__logo}`}><span>Nu</span> Kenzie</h1>
+    </header>
   )
 }
 
